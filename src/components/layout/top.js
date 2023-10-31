1d36bf24b3c247eb2a0ff4301a05482b55195d6c
Vivek-Sakthivel92 <vsakthivel@hexalytics.com>
2023-10-31 15:35:31 +0530

    first commit to main branch

---
+++ src/components/layout/top.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default function Top() {
+    
+    return (        
+        <header>
+            <div className='text-center'>Header</div>
+        </header>
+    )
+
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Head from 'next/head';
+import Layout from '@/components/layout/layout';
+
+export default function Index() {
+
+  return (
+    <>
+      <Layout pageTitle="Home" />
+    </>
+
+  );
+}
